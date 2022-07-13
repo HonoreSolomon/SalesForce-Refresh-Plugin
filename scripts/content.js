7fba775e7e65784a415b87084289ad64fea0f9d8
HonoreSolomon <HonoreSolomon@hotmail.com>
2022-07-13 15:42:36 -0700

    changed selector to new value using only classes

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,5 @@
 // Confirm script is injected
 
-
 //initial interval value
 let intervalId;
 
@@ -8,62 +7,55 @@ let intervalId;
 
 function clickRefreshButtonProcess() {
   if (intervalId) {
-    let refreshButton = null
+    let refreshButton = null;
     // Grab frame containing refresh button
-    let iframeEl = document.querySelector("#brandBand_2 > div > div > div > div > div.dashboardContainer > iframe");
-    
+    const iframeEl = document.querySelector(
+      ".dashboardContainer [title=dashboard]"
+    );
+
     if (iframeEl) {
-      
-      refreshButton = iframeEl.contentDocument.querySelector("button.slds-button.refresh");
-
+      refreshButton = iframeEl.contentDocument.querySelector(
+        ".dashboardHeader .actionRibbon .actions .slds-button.refresh"
+      );
     }
-    
-    if (refreshButton) {
+    if (!iframeEl) {
+      console.log("error loading iframe");
+    }
 
+    if (refreshButton) {
       // checks if button is enabled
       if (!refreshButton.disabled) refreshButton.click();
-
+      console.log("success");
       // if button is disabled then there will be a force refresh of entire page
       if (refreshButton.disabled) iframeEl.contentWindow.location.reload();
     }
   }
-  
-  if (!intervalId) {
 
+  if (!intervalId) {
+    console.log("error");
   }
 }
 
 //clears interval id restarting interval for new value
 function clearRefreshInterval() {
-
-
   clearInterval(intervalId);
-  
+
   //clear interval Id from varriable
-  
+
   intervalId = null;
-
-
 }
 
 //sets interval id and also sets the interval
 function setRefreshInterval(refreshInterval) {
-  
   intervalId = setInterval(clickRefreshButtonProcess, refreshInterval);
-
 }
 
 //grabs value varriable from storage and sets it to refresh interval and calls refresh interval function
 function getTimerSelectValue() {
-  
   chrome.storage.local.get(["refreshTimerSelectValue"], function (result) {
+    const refreshInterval = parseInt(result.refreshTimerSelectValue) * 1000;
 
-
-    let refreshInterval = parseInt(result.refreshTimerSelectValue) * 1000;
-
-
-
-
+    console.log("in");
     setRefreshInterval(refreshInterval);
   });
 }
@@ -71,23 +63,15 @@ function getTimerSelectValue() {
 function pluginProcess() {
   //checks storage to see if refresh is enabled
   chrome.storage.local.get(["isRefreshEnabled"], function (result) {
-    
-
-
-    let toggleRefresh = result.isRefreshEnabled;
-
+    const toggleRefresh = result.isRefreshEnabled;
 
     //if refresh is enabled the proccess continues
     if (toggleRefresh) {
-
-
       clearRefreshInterval();
 
       getTimerSelectValue();
     }
     if (!toggleRefresh) {
-
-
       clearRefreshInterval();
     }
   });
@@ -98,7 +82,5 @@ pluginProcess();
 
 // on change to storage starts plugin process again
 chrome.storage.onChanged.addListener(function () {
-  
   pluginProcess();
-
 });
