47c2ff2d83ed7f0745907ee7f18bc5714f915f4f
Honore Solomon <104269819+HonoreSolomon@users.noreply.github.com>
2023-01-26 17:02:55 -0800

    "Refactor and document storage management code"

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -1,49 +1,39 @@
-// Init the toggle value
-
+// Initialize the toggle value on page load
 chrome.storage.local.get(["isRefreshEnabled"], function (result) {
   const storedRefreshCheckedValue = result.isRefreshEnabled;
 
+  // Get the DOM element for the toggle
   const refreshEnableEl = document.getElementById("toggle");
 
+  // Check if there is a stored value and set the toggle accordingly
   if (storedRefreshCheckedValue) {
     refreshEnableEl.checked = storedRefreshCheckedValue;
-  }
-
-  if (!storedRefreshCheckedValue) {
+  } else {
+    // If no stored value, set the toggle to false and store the value
     refreshEnableEl.checked = false;
-
-    console.log(refreshEnableEl.checked);
-
     chrome.storage.local.set({ isRefreshEnabled: refreshEnableEl.checked });
   }
 
   console.log(`isRefreshEnabled: ${result.isRefreshEnabled}`);
 });
 
-// Init the select value
-
+// Initialize the select value on page load
 chrome.storage.local.get(["refreshTimerSelectValue"], function (result) {
   const storedValue = result.refreshTimerSelectValue;
 
+  // Get the DOM element for the select
   const refreshTimerSelectEl = document.getElementById("time");
 
+  // Check if there is a stored value and set the select accordingly
   if (storedValue) {
     refreshTimerSelectEl.value = storedValue;
-  }
-
-  if (!storedValue) {
+  } else {
+    // If no stored value, set the select to 90 and store the value
     refreshTimerSelectEl.value = 90;
-
-    console.log(refreshTimerSelectEl.value);
-
-    chrome.storage.local.set({
-      refreshTimerSelectValue: refreshTimerSelectEl.value,
-    });
+    chrome.storage.local.set({ refreshTimerSelectValue: refreshTimerSelectEl.value });
   }
 
-  console.log(
-    `setRefreshTimerDropdownValue: ${result.refreshTimerSelectValue}`
-  );
+  console.log(`setRefreshTimerDropdownValue: ${result.refreshTimerSelectValue}`);
 });
 
 // Store checked value on refresh enable button
